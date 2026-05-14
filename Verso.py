330e3b856dc3f7a4d8e43c9a28381ecbf5c363b0
yaseenmind11-art <yaseen.mind11@gmail.com>
2026-05-14 14:07:28 +0300

    Update Verso.py

--- Verso.py
+++ Verso.py
@@ -145,7 +145,7 @@ st.markdown(f"""
     <style>
     .stApp {{ color: inherit; }}
       .notebook-card {{ 
-        background-color: #A9A9A9.; 
+        background-color: #000080; 
         padding: 30px; border-radius: 12px; border-left: 6px solid {accent}; 
         margin-bottom: 15px; color: #FFFFFF !important; box-shadow: 0 4px 10px -1px rgb(0 0 0 / 0.2);
     }}
