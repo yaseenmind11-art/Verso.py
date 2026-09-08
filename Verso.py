5d3b36314ad09465c5cca0f04c6d0c913129ef2d
yaseenmind11-art <yaseen.mind11@gmail.com>
2026-09-08 18:45:05 +0300

    Update Verso.py

--- Verso.py
+++ Verso.py
@@ -16,7 +16,7 @@ import urllib3
 import os  
 from google import genai
 from google.genai import types
-from deep-translator
+import deep-translator
 # Disable insecure request warnings if connection requires SSL bypass on a managed proxy network
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
